4e0b680c0c7a01c72615937dfa419d39a334dd93
Andor Saga <andor.saga@gmail.com>
2021-08-24 18:43:55 -0400

    cleanup.

--- README.md
+++ README.md
@@ -1,28 +1,22 @@
 # p5.floodFill
 Flood fill area of a sketch.
 
-<img src="img.png" width="200"/>
+<img src="img.jpg" width="500"/>
 
 ## Usage
 ```js
- new p5(p => {
-    let gfx;
-
+new p5(p => {
     p.setup = _ => {
         p.createCanvas(500, 500);
-        p.background(255);
+        p.pixelDensity(1);
+        p.randomSeed(0);
+        p.noFill();
 
-        gfx = p.createGraphics(p.width, p.height);
-    }
+        for (let i = 0; i < 20; i++) {
+            p.ellipse(p.random(p.width), p.random(p.height), p.random(400, 1500));
+        }
 
-    p.draw = _ => {
-        let c = color(33, 66, 99);
-        gfx.flooFill(100, 100, c);
-    }
-
-    p.mousePressed = _ => {
-        
-        gfx.filter(p.BLUR, 1);
+        p.floodFill(p.width / 2, p.height / 2, p.color(33, 66, 99));
     }
 });
 ```
--- src/index.js
+++ src/index.js
@@ -4,32 +4,44 @@
 
     let p5 = window.p5;
 
-    p5.prototype.floodFill = function (gfx, x, y, col, predicate) {
+    /*
+        arguments: x, y, color
+        OR
+        arguments: gfx, x, y, color
+    */
+    p5.prototype.floodFill = function (a, b, c, d) {
+
+        let gfx, x, y, col;
+        let w, h;
+        let px;
+
+        // using args:  x, y, col
+        if (typeof (a) == 'number') {
+            gfx = this;
+            [x, y, col] = [a, b, c];
+        }
+        // using args:  gfx, x, y, col
+        else {
+            [gfx, x, y, col] = [a, b, c, d];
+        }
+
+        gfx.loadPixels();
+        px = gfx.pixels;
+        [w, h] = [gfx.width, gfx.height];
 
         x = p5.prototype.floor(x);
         y = p5.prototype.floor(y);
-
-        gfx.loadPixels();
-
-        let px;
-
-        // if passed in main gfx
-        if (gfx._pixelsState.pixels.length > 0) {
-            px = gfx._pixelsState.pixels;
-        } else {
-            px = gfx.pixels;
-        }
 
         let idx = getIdx(x, y);
         let target = [px[idx + 0], px[idx + 1], px[idx + 2], px[idx + 3]];
         let stack = [];
 
         function hitEdgeOfCanvas(top) {
-            return top.x < 0 || top.y < 0 || top.x > gfx.width - 1 || top.y > gfx.height - 1
+            return top.x < 0 || top.y < 0 || top.x > w - 1 || top.y > h - 1
         }
 
         function getIdx(x, y) {
-            return (y * gfx.width + x) * 4;
+            return (y * w + x) * 4;
         }
 
         function matchesTarget(x, y) {
@@ -41,10 +53,9 @@
                 p5.prototype.abs(target[3] - px[idx + 3]) < 50;
         }
 
-        if (x && y) {
-            stack.push({ x, y });
-        }
+        stack.push({ x, y });
 
+        let numPixels = 0;
         while (stack.length > 0) {
 
             let top = stack[stack.length - 1];
@@ -59,7 +70,10 @@
             px[idx + 0] = p5.prototype.red(col);
             px[idx + 1] = p5.prototype.green(col);
             px[idx + 2] = p5.prototype.blue(col);
-            px[idx + 3] = px[idx + 3] > 0 ? px[idx + 3] : 255;
+            let a = col.levels[3];
+
+            px[idx + 3] = px[idx + 3] > 0 ? px[idx + 3] : a;
+            numPixels++;
 
             stack.push({ x: top.x, y: top.y + 1 });
             stack.push({ x: top.x - 1, y: top.y });
@@ -67,6 +81,7 @@
             stack.push({ x: top.x, y: top.y - 1 });
         }
         gfx.updatePixels();
+        return numPixels;
     };
 
 })();
